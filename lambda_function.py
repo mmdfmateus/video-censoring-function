5e3d6e105a14bf5bb68aa68e9f7f776a3049abbf
mmdfmateus <mmdfmateus@gmail.com>
2024-03-21 21:51:24 -0300

    add postback call

--- lambda_function.py
+++ lambda_function.py
@@ -3,18 +3,22 @@ import os
 import subprocess
 import shlex
 import boto3
+import urllib3
 
 S3_DESTINATION_BUCKET = "ugcvideo-destination-bucket"
 SIGNED_URL_TIMEOUT = 6000
 FFMPEG_STATIC = "/tmp/ffmpeg"
 TMP_DESTINATION = "/tmp/"
 
+POSTBACK_ENDPOINT = "https://typedwebhook.tools/webhook/33ef2702-d666-49ce-a1a3-47035599de62"
+
 os.system("cp -ra ./ffmpeg /tmp/")
 os.system("cp -ra ./brfutsal.png /tmp/")
 os.system("chmod -R 775 /tmp")
 
-def lambda_handler(event, context):
+http = urllib3.PoolManager()
 
+def lambda_handler(event, context):
     s3_source_bucket = event['Records'][0]['s3']['bucket']['name']
     s3_source_key = event['Records'][0]['s3']['object']['key']
 
@@ -26,6 +30,8 @@ def lambda_handler(event, context):
         Params={'Bucket': s3_source_bucket, 'Key': s3_source_key},
         ExpiresIn=SIGNED_URL_TIMEOUT)
         
+    object_head = s3_client.head_object(Bucket=s3_source_bucket, Key=s3_source_key)
+
     cmd = "ls -la /tmp"
     returned_output = subprocess.check_output(cmd.split())
     result = returned_output.decode("utf-8")
@@ -69,6 +75,9 @@ def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(S3_DESTINATION_BUCKET)
     bucket.upload_file(complete_destination, s3_destination_filename)
+
+    encoded_body = json.dumps(object_head['Metadata'])
+    r = http.request('POST', POSTBACK_ENDPOINT, headers={'Content-Type': 'application/json'}, body=encoded_body)
 
     return {
         'statusCode': 200,
